8f1ec354b4e457dd058cc9d904ce37923876959e
apeart30 <murengernatwarijames@gmail.com>
2023-10-10 17:06:21 -0700

    all new

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * alphabets are going to be printed
+ * Return:0 
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+
+}
