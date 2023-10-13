aa40b14c0a2abb8586cb37bf3b2c855060436688
apeart30 <murengernatwarijames@gmail.com>
2023-10-12 18:41:54 -0700

    completed

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,4 +10,5 @@ extern void more_numbers(void);
 extern void print_line(int n);
 extern void print_diagonal(int n);
 extern void print_square(int size);
+extern void print_triangle(int size);
 #endif
